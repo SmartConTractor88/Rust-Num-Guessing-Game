9b189c74bd78e7dfab6879d3935fbc8bb4e32804
SmartConTractor <login.blockchain888@gmail.com>
2025-05-05 14:57:43 +0200

    Compare input with the generated number.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 use std::io; // input/output from standard library
+use std::cmp::Ordering; // for comparing values
 use rand::Rng;
 
 fn main() {
 
  println!("---- Guess the Number Between 1 and 100 ---");
 
- let secret_number = rand::thread_rng().gen_range(1..=100);
+ let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
     
  println!("Please input your guess.");
  let mut guess = String::new();
  io::stdin()
  .read_line(&mut guess)
  .expect("Failed to read line");
- print!("You guessed: {}", guess);
- println!("The secret number was {secret_number}");
 
+ let guess: u32 = guess
+ .trim()// remove whitespace
+ .parse() // convert to the specified type (u32)
+ .expect("Please type a number!");
+
+ println!("You guessed: {}", guess);
+
+ match guess.cmp(&secret_number) {
+    Ordering::Less => println!("Too small!"),
+    Ordering::Greater => println!("Too big!"),
+    Ordering::Equal => println!("You win!"),
+    }
+    println!("The secret number was: {}", secret_number);
 }

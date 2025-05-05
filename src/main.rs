d6d0d8eb83ea6cc6b00b4965ced0bfe8adaac38e
SmartConTractor <login.blockchain888@gmail.com>
2025-05-05 14:11:54 +0200

    Generate a random number. Print it after guessing.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::io; // input/output from standard library
+use rand::Rng;
 
 fn main() {
 
- println!("Guess the number!");
+ println!("---- Guess the Number Between 1 and 100 ---");
+
+ let secret_number = rand::thread_rng().gen_range(1..=100);
+    
  println!("Please input your guess.");
  let mut guess = String::new();
  io::stdin()
  .read_line(&mut guess)
  .expect("Failed to read line");
- println!("You guessed: {}", guess);
+ print!("You guessed: {}", guess);
+ println!("The secret number was {secret_number}");
+
 }
